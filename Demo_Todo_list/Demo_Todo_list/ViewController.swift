717c8caee578d844b6cca61215d0cc2303da20f8
Rajeshnath Chyarngayil Vishwanath <rajeshn597@gmail.com>
2018-10-18 16:49:19 +0530

    stage 1 : inserting and fetch basic implmentation

--- Demo_Todo_list/Demo_Todo_list/ViewController.swift
+++ Demo_Todo_list/Demo_Todo_list/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedObjectContext = appDelegate.persistentContainer.viewContext
+        
+        
+        guard let todoEntity = NSEntityDescription.entity(forEntityName: "Todo", in: managedObjectContext) else {
+            return
+        }
+        
+        let todo = NSManagedObject(entity: todoEntity, insertInto: managedObjectContext)
+        
+        todo.setValue("Service the car", forKey: "notes")
+        
+        // saving the data
+        
+        do {
+            try managedObjectContext.save()
+        } catch {
+            print("Could not save error")
+        }
+        
+        // fetching the data
+        
+        let fetchNotes = NSFetchRequest<NSFetchRequestResult>(entityName: "Todo")
+        
+        do {
+            guard let notes = try managedObjectContext.fetch(fetchNotes) as? [Todo] else {
+                return
+            }
+            for note in notes {
+                print(note.notes)
+                
+            }
+            
+        } catch {
+            print("Error in fetching")
+        }
     }
-
 
 }
 
